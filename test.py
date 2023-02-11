1af6058da8d02092497f1159fa82d111a31d65e9
ZhangSanbaoo <melub.the.magician@gmail.com>
2023-02-11 21:42:30 +0100

    tp2professionprogrammingdone

--- test.py
+++ test.py
@@ -1,30 +1,5 @@
-# def bubbleSort(array):
-#     isSorted = True
-#     while isSorted == True :
-#         isSorted = True
-#         count = 0
-#         for count in range(len(array)-1) :
-#             if array[count] > array[count+1]:
-#                 a = array[count]
-#                 array[count] = array[count+1]
-#                 array[count+1] = a
-#                 isSorted = False
-#     pass
+dataRange = 10
+dataSize = 10
 
-
-def bubbleSort(array):
-    isSorted = True
-    while isSorted:
-        isSorted = True
-        for i in range(len(array)-1):
-            if array[i] > array[i+1]:
-                array[i], array[i+1] = array[i+1], array[i]
-                isSorted = False
-        return array
-array = [6,4,9,12,1]
-
-bubbleSort(array)
-print(array)
-
-a = 12
-b = 54
+worst = list(reversed(range(0,dataSize)))
+print(worst)
