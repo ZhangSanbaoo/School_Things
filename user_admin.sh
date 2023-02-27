be545338de199b34c77a1c3d17c375f553f11e12
ZhangSanbaoo <melub.the.magician@gmail.com>
2023-02-27 15:06:35 +0100

    m

--- user_admin.sh
+++ user_admin.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-
+echo -e "Welcome in EPF Users’ administration Super Script \nScript by WANG. v20230227.1"
+echo "1 – Show user accounts present on the system"
+echo "2 – Test if a user account is present on the system"
+echo "3 – Quit the program"
+read selection
+case $selection in
+    [1])
+    ;;
+    [2])
+    ;;
+    [3])
+    ;;
+esac
 exit 0
