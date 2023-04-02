b5c23a71c1d72a1826e01e88ebc6d983b64b01dc
Zhangsanbaoo <melub.the.magician@gmail.com>
2023-04-02 19:00:29 +0200

    e

--- ocr.py
+++ ocr.py
@@ -4,5 +4,5 @@ import torchvision
 import numpy as np
 
 reader = easyocr.Reader(['fr'])
-result = reader.readtext('9c37a5b684a29cdac4e1dacf9aa86e49.jpg', detail = 0, paragraph = True, gpu = False)
+result = reader.readtext('9c37a5b684a29cdac4e1dacf9aa86e49.jpg', detail = 0, paragraph = True)
 print(result)
