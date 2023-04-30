55f32e45a69d1179a03ae948db03b4e5e22a668c
ZhangSanbaoo <melub.the.magician@gmail.com>
2023-04-30 14:36:58 +0200

    w

--- test.py
+++ test.py
@@ -1,53 +1,6 @@
-# import matplotlib
-# # matplotlib.use('Qt5Agg')
-# matplotlib.use('TkAgg')
-# print(matplotlib.get_backend())
-
-# import matplotlib.pyplot as plt
-# # import tkinter
-
-# # import pandas as pd
-# # import numpy as np
-
-# # from numpy import *
-# # matplotlib.use('Qt5Agg')
-
-# def main():
-#     x = []
-#     y = []
-#     for i in range(-100, 100):
-#         x.append(i)
-#         y.append(i*i)
-#     plt.plot(x, y)
-#     plt.show()
-# main()
-
-import random
-
-count1 = 0
-count2 = 0
-count3 = 0
-count4 = 0
-
-while True:
-    a = random.randint(1,4)
-    if a == 1:
-        count1+=1
-    if a == 2:
-        count2+=1
-    if a == 3:
-        count3+=1
-    if a == 4:
-        count4+=1
-    if count1 >= 5:
-        print('1')
-        break
-    if count2 >= 5:
-        print('2')
-        break
-    if count3 >= 5:
-        print('3')
-        break
-    if count4 >= 5:
-        print('4')
-        break
+version: '3'
+services:
+    jupyter:
+        image: jupyter/minimal-notebook
+        ports:
+            - "8888:8888"
