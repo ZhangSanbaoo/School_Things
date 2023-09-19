a868d16df658ce0c6d1bf47922a01062df517aab
ZhangSanbaoo <melub.the.magician@gmail.com>
2023-09-19 11:47:05 +0200

    geodata

--- ocr.py
+++
@@ -1,9 +0,0 @@
-import easyocr
-import torch
-import torchvision
-import numpy as np
-
-reader = easyocr.Reader(['fr'])
-# result = reader.readtext('9c37a5b684a29cdac4e1dacf9aa86e49.jpg', detail = 0, paragraph = True)
-result = reader.readtext('111.jpg', detail = 0, paragraph = True)
-print(result)
